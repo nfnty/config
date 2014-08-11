ae836aa888f4cf7e714e6853afe558fe145a0ee7
nfnty <git@nfnty.se>
2014-08-11 08:41:28 +0200

    fixed paths

--- sync.sh
+++ sync.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 mkdir -p ~/.config
-rsync -a --delete .config/htop ~/.config/htop
+rsync -a --delete .config/htop ~/.config
 
-rsync -a --delete .vim ~/.vim --exclude='/undodir'
+rsync -a --delete .vim ~ --exclude='/undodir'
 ln -sf .vim/vimrc ~/.vimrc
 
-rsync -a --delete .zprezto ~/.zprezto
+rsync -a --delete .zprezto ~
 ln -sf .zprezto/runcoms/zlogin		~/.zlogin
 ln -sf .zprezto/runcoms/zpreztorc	~/.zpreztorc
 ln -sf .zprezto/runcoms/zprofile	~/.zprofile
