96218ecac1ea77341a919806a3228ba054002987
nfnty <git@nfnty.se>
2014-10-14 01:21:38 +0200

    fixed screen.conf path

--- sync.sh
+++ sync.sh
@@ -3,8 +3,7 @@
 mkdir -p "$HOME/.config"
 
 rsync -a --delete '.config/htop' "$HOME/.config"
+rsync -a --delete '.config/screen.conf' "$HOME/.config"
 
 rsync -a --delete '.config/zsh' "$HOME/.config" --exclude='zsh/.zdirs' --exclude='zsh/.zhistory'
 ln -sf '.zprezto/runcoms/zshenv' "$HOME/.config/zsh/.zprezto/runcoms/zshenv"
-
-rsync -a --delete '.screenrc' "$HOME"
