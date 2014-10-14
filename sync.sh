c1a577525f5dcd73749b7e7b3191fb13754bb985
nfnty <git@nfnty.se>
2014-10-14 03:14:49 +0200

    fixed sync script

--- sync.sh
+++ sync.sh
@@ -6,4 +6,6 @@ rsync -a --delete '.config/htop' "$HOME/.config"
 rsync -a --delete '.config/screen.conf' "$HOME/.config"
 
 rsync -a --delete '.config/zsh' "$HOME/.config" --exclude='zsh/.zdirs' --exclude='zsh/.zhistory'
-ln -sf '.zprezto/runcoms/zshenv' "$HOME/.config/zsh/.zprezto/runcoms/zshenv"
+ln -sf ".config/zsh/.zprezto/runcoms/zshenv" "$HOME/.zshenv"
+
+chown -R "$UID:$UID" "$HOME/.config"
